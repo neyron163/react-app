82a56902f4625fe5bb666373639a54a0dcf50204
Andrey Williams <andrey163783@gmail.com>
2018-10-26 10:26:52 -0700

    auth

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./db');
+
+const users = require('./routes/user'); 
+
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    () => {console.log('Database is connected') },
+    err => { console.log('Can not connect to the database'+ err)}
+);
 
 const app = express();
-const port = process.env.PORT || 5000;
+app.use(passport.initialize());
+require('./routes/passport')(passport);
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api/users', users);
+
+app.get('/', function(req, res) {
+    res.send('hello');
 });
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+const PORT = process.env.PORT || 5000;
 
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
