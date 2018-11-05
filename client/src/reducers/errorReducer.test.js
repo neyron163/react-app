18d5fc9240a7b9f98d2e2aea76ca5a5544076748
Andrey Williams <andrey163783@gmail.com>
2018-11-04 23:59:18 -0800

    erros test update

--- client/src/reducers/errorReducer.test.js
+++ client/src/reducers/errorReducer.test.js
@@ -1,19 +1,42 @@
 import reducer from './errorReducer';
 import * as types from '../actions/types';
 
+describe('authorization reducer', () => {
 
-describe('error reducer', () => {
-  it(' reducer for GET_ERRORS ', () => {
-    let state = {
-      error: {
-        errors: 'errors'
-      }
-    }
-    state = reducer(state, types.GET_ERRORS)
-    expect(state).toEqual({
-      error: {
-        errors: 'errors'
-      }
-    })
-  })
-})
+  it('should return the initial state', () => {
+    const state = undefined;
+    const action = {}
+    expect(reducer(state, action)).toEqual({});
+  });
+
+  it('should handle GET_ERRORS action', () => { 
+    const state = {};
+    const action = {
+      type: types.GET_ERRORS,
+      payload: {
+        title: 'Title is empty',
+        description: 'Description is empty'
+      } 
+    };
+
+
+    expect(reducer(state, action)).toEqual({
+        title: 'Title is empty',
+        description: 'Description is empty'
+    });
+
+
+    expect(reducer(state, {
+      type: types.GET_ERRORS,
+      payload:{
+        login: 'Login is empty',
+        password: 'Password is empty'
+      } 
+    })).toEqual({
+        login: 'Login is empty',
+        password: 'Password is empty'
+    });
+
+  });
+
+});
