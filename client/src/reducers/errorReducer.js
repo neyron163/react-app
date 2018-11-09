986639681358799af787409bd814d06d8f07f39e
Andrey Williams <andrey163783@gmail.com>
2018-11-09 07:08:31 -0800

    error flow reducer

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,8 +1,21 @@
+// @flow
+
 import { GET_ERRORS } from '../actions/types';
+
+
+type State = Object;
+  
+  type TYPE_ERROR = { type: GET_ERRORS, payload: Array<{
+    title: string,
+    description: string,
+  }> };
+  
+  type Action = TYPE_ERROR;
+
 
 const initialState = {};
 
-export default function(state = initialState, action ) {
+export default function(state: State = initialState, action: Action) {
     switch(action.type) {
         case GET_ERRORS:
             return action.payload;
