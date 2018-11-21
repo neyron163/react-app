cec25fbeacd50ee7b216946648a65e9f83d76726
Andrey Williams <andrey163783@gmail.com>
2018-11-21 00:06:36 -0800

    first user admin

--- routes/user.js
+++ routes/user.js
@@ -9,16 +9,18 @@ const validateLoginInput = require('../validation/login');
 
 const User = require('../models/User');
 
-router.post('/register', function(req, res) {
+router.post('/register', function (req, res) {
     const { errors, isValid } = validateRegisterInput(req.body);
 
-    if(!isValid) {
+    if (!isValid) {
         return res.status(400).json(errors);
     }
+
+
     User.findOne({
         email: req.body.email
     }).then(user => {
-        if(user) {
+        if (user) {
             return res.status(400).json({
                 email: 'Email already exists'
             });
@@ -29,27 +31,41 @@ router.post('/register', function(req, res) {
                 r: 'pg',
                 d: 'mm'
             });
-            const newUser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password,
-                avatar
-            });
-            
-            bcrypt.genSalt(10, (err, salt) => {
-                if(err) console.error('There was an error', err);
-                else {
-                    bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if(err) console.error('There was an error', err);
-                        else {
-                            newUser.password = hash;
-                            newUser
-                                .save()
-                                .then(user => {
-                                    res.json(user)
-                                }); 
-                        }
-                    });
+
+            function createUser(access) {
+
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                    access: access,
+                    avatar
+                });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    if (err) console.error('There was an error', err);
+                    else {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) console.error('There was an error', err);
+                            else {
+                                newUser.password = hash;
+                                newUser
+                                    .save()
+                                    .then(user => {
+                                        res.json(user)
+                                    });
+                            }
+                        });
+                    }
+                });
+            }
+
+            // Проверка количества зарегестрированых пользователей, первому пользователю выдаем права администратора
+            User.find({}, function (el, users) {
+                if (!users.length) {
+                    createUser(10);
+                } else {
+                    createUser(0);
                 }
             });
         }
@@ -60,45 +76,45 @@ router.post('/login', (req, res) => {
 
     const { errors, isValid } = validateLoginInput(req.body);
 
-    if(!isValid) {
+    if (!isValid) {
         return res.status(400).json(errors);
     }
 
     const email = req.body.email;
     const password = req.body.password;
 
-    User.findOne({email})
+    User.findOne({ email })
         .then(user => {
-            if(!user) {
+            if (!user) {
                 errors.email = 'User not found'
                 return res.status(404).json(errors);
             }
             bcrypt.compare(password, user.password)
-                    .then(isMatch => {
-                        if(isMatch) {
-                            const payload = {
-                                id: user.id,
-                                name: user.name,
-                                avatar: user.avatar,
-                                access: user.access
+                .then(isMatch => {
+                    if (isMatch) {
+                        const payload = {
+                            id: user.id,
+                            name: user.name,
+                            avatar: user.avatar,
+                            access: user.access
+                        }
+                        jwt.sign(payload, 'secret', {
+                            expiresIn: 3600
+                        }, (err, token) => {
+                            if (err) console.error('There is some error in token', err);
+                            else {
+                                res.json({
+                                    success: true,
+                                    token: `Bearer ${token}`
+                                });
                             }
-                            jwt.sign(payload, 'secret', {
-                                expiresIn: 3600
-                            }, (err, token) => {
-                                if(err) console.error('There is some error in token', err);
-                                else {
-                                    res.json({
-                                        success: true,
-                                        token: `Bearer ${token}`
-                                    });
-                                }
-                            });
-                        }
-                        else {
-                            errors.password = 'Incorrect Password';
-                            return res.status(400).json(errors);
-                        }
-                    });
+                        });
+                    }
+                    else {
+                        errors.password = 'Incorrect Password';
+                        return res.status(400).json(errors);
+                    }
+                });
         });
 });
 
